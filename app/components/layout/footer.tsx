de0a36097b5a87d8eebe4bcf54b4f99d47f45840
Jake Lipson <jake_lipson@yahoo.com>
2024-02-23 11:26:13 -0500

    Removed top border from footer

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
     return (
         <div className="container">
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top border-secondary">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-secondary">
                 <div className="col-md-4 d-flex align-items-center">
                     <a href="/" className="mb-3 me-2 mb-md-0 mt-2 text-secondary text-decoration-none lh-1">
                         JL
