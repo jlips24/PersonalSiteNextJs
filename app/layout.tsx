71a6066c87e3b775dfa9190b85fd93b7fc05cf1d
Jake Lipson <jake_lipson@yahoo.com>
2024-02-23 10:25:45 -0500

    added vercel speed analytics to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import './globals.css'
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
