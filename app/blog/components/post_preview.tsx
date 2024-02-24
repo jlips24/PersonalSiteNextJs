e2abda0efb4946878e4d032fb24e693fc2f25ced
Jake Lipson <jake_lipson@yahoo.com>
2024-02-24 01:39:31 -0500

    started designing blog frontend

---
+++ app/blog/components/post_preview.tsx
@@ -0,0 +1,19 @@
+export default function PostPreview () {
+    return (
+        <div className="card my-3 bg-dark font-monospace">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="/images/job_headers/spotify.jpg" className="img-fluid rounded h-100" style={{ objectFit: "cover" }} alt="..." />
+                    {/* TODO: Use <Image> tage here instead of <img> */}
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body bg-dark text-light">
+                        <h5 className="card-title">Post title</h5>
+                        <p><small>February 24th, 2024</small></p>
+                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,17 @@
+import PostPreview from "./components/post_preview";
+
 export default function Blog() {
     return (
-    <>
-    <div className="container">
-        <h1 className="font-monospace">Welcome to the Blog</h1>
-        <p className="font-monospace text-light-gray">Under construction</p>
-    </div>
-    </>
+        <div className="container">
+            <h1 className="font-monospace">
+                Welcome to the Blog
+            </h1>
+            <p className="font-monospace text-light-gray">
+                Under construction
+            </p>
+            <PostPreview />
+            <PostPreview />
+            <PostPreview />
+        </div>
     )
 }
