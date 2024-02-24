8e65316f85804cf6ca0552b27cda40ea91b2b549
Jake Lipson <jake_lipson@yahoo.com>
2024-02-23 22:58:12 -0500

    Change navbar collapse button to thheme color (orange)

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
             <a className="navbar-brand text-secondary fw-lighter" href="/">J L</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler navbar-toggler-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon text-secondary"></span>
             </button>
                 <div className="collapse navbar-collapse " id="navbarNav">
