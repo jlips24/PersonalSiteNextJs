645420be9864c626226405ffda0f928395c24860
Jake Lipson <jake_lipson@yahoo.com>
2024-06-08 01:05:32 -0400

    moved key

--- app/components/photography/categories.tsx
+++ app/components/photography/categories.tsx
@@ -12,8 +12,8 @@ export default function Categories() {
     return (
         <div className="container p-5 mb-4 g-3 text-tangerine bg-dark">
             {categories.map(category => (
-                <div>
-                    <h4 key={category}>{category}</h4>
+                <div key={category}>
+                    <h4>{category}</h4>
                     <hr/>
                 </div>
             ))}
